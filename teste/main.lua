177d06a9a68fa5c909adf9d613a47592f16bc5a1
Rick Schneider <otaviohenrique.dev@gmail.com>
2022-07-06 13:34:08 -0300

    Exercicio de teste curso de LUA

---
+++ teste/main.lua
@@ -0,0 +1,25 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+-- Your code here
+Plano = 0
+S = "isso é uma variavel global"
+print (S)
+
+
+local fogo = "isso é uma variavel local"
+print (fogo)
+
+local valor1 = 1
+print("valor1 = 1")
+local valor2 = 2
+print("valor2 = 2")
+
+local sum = valor1 + valor2
+
+print("valor1 mais valor 2 é = ".. sum)
+print("-------------------------------")
+
+
